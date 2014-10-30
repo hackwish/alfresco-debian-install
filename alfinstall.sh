7fae024a1b9ad97d50d9ce3939c5cc8b5af55087
Yannick MOLINET <yannick.molinet@dixinfor.com>
2014-10-30 23:45:48 +0100

    update sed

--- alfinstall.sh
+++ alfinstall.sh
@@ -514,13 +514,13 @@ echo
     $SUDO curl -# -o $ALF_HOME/addons/apply.sh $BASE_DOWNLOAD/scripts/apply.sh
     $SUDO chmod u+x $ALF_HOME/addons/apply.sh
 
-	sed -i.bak -e "s;ALF_HOME=*$;ALF_HOME=${ALF_HOME}/g" $ALF_HOME/addons/apply.sh
-	sed -i.bak -e "s;CATALINA_HOME=*$;CATALINA_HOME=${CATALINA_HOME}/g"  $ALF_HOME/addons/apply.sh
-	sed -i.bak -e "s;CATALINA_BASE=*$;CATALINA_BASE=${CATALINA_BASE}/g"  $ALF_HOME/addons/apply.sh
-	sed -i.bak -e "s;CATALINA_CONF=*$;CATALINA_CONF=${CATALINA_CONF}/g"  $ALF_HOME/addons/apply.sh	
-	sed -i.bak -e "s;CATALINA_PID=*$;CATALINA_PID=${CATALINA_PID}/g"  $ALF_HOME/addons/apply.sh
-	sed -i.bak -e "s;ALF_USER=*$;ALF_USER=${ALF_USER}/g"  $ALF_HOME/addons/apply.sh
-
+	sed -i.bak -e "s;ALF_HOME=*$;ALF_HOME=${ALF_HOME};g" $ALF_HOME/addons/apply.sh
+	sed -i.bak -e "s;CATALINA_HOME=*$;CATALINA_HOME=${CATALINA_HOME};g"  $ALF_HOME/addons/apply.sh
+	sed -i.bak -e "s;CATALINA_BASE=*$;CATALINA_BASE=${CATALINA_BASE};g"  $ALF_HOME/addons/apply.sh
+	sed -i.bak -e "s;CATALINA_CONF=*$;CATALINA_CONF=${CATALINA_CONF};g"  $ALF_HOME/addons/apply.sh	
+	sed -i.bak -e "s;CATALINA_PID=*$;CATALINA_PID=${CATALINA_PID};g"  $ALF_HOME/addons/apply.sh
+	sed -i.bak -e "s;ALF_USER=*$;ALF_USER=${ALF_USER};g"  $ALF_HOME/addons/apply.sh
+	
 	fi
   if [ ! -f "$ALF_HOME/addons/alfresco-mmt.jar" ]; then
     $SUDO curl -# -o $ALF_HOME/addons/alfresco-mmt.jar $BASE_DOWNLOAD/scripts/alfresco-mmt.jar
