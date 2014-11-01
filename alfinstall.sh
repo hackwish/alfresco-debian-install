37ab8d70585896b9e97b90b12999703eb6a0abf9
Yannick MOLINET <yannick.molinet@dixinfor.com>
2014-11-01 18:43:15 +0100

    Fix err:        Cannot run program "/opt/libreoffice4.2/program/soffice.bin": error=2, Aucun fichier ou dossier de ce type by updating path in alfresco-global.properties

--- alfinstall.sh
+++ alfinstall.sh
@@ -431,6 +431,7 @@ read -e -p "Install LibreOffice${ques} [y/n] " -i "y" installibreoffice
 if [ "$installibreoffice" = "y" ]; then
 	if [ "$usepack" = "y" ]; then
 		$SUDO apt-get $APTVERBOSITY install libreoffice
+		$OOOEXE="/usr/lib/libreoffice/program/soffice.bin"
 	else
 		cd /tmp/alfrescoinstall
 		curl -# -L -O $LIBREOFFICE
@@ -438,7 +439,12 @@ if [ "$installibreoffice" = "y" ]; then
 		cd "$(find . -type d -name "LibreOffice*")"
 		cd DEBS
 		$SUDO dpkg -i *.deb
+		$OOOEXE="/opt/libreoffice4.2/program/soffice.bin"
 	fi
+	echoblue "Update alfresco-global.properties"
+	sed -i.bak -e "s;ooo.exe=.*;ooo.exe=$OOOEXE;g" $CATALINA_BASE/shared/classes/alfresco-global.properties
+	echo
+	
 	echo
 	echoblue "Installing some support fonts for better transformations."
 	$SUDO apt-get $APTVERBOSITY install ttf-mscorefonts-installer fonts-droid
